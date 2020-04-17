7c5b86318cb2918a28d1b38140477e416d747447
Nicolas Agostini <n.b.agostini@gmail.com>
2020-04-17 09:59:11 -0400

    Require user login to access About and Graphs

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView 
+from .views import HomePageView, AboutPageView, GraphsPageView
 
 
 urlpatterns = [
+    path('graphs/', GraphsPageView.as_view(), name='graphs'), 
     path('about/', AboutPageView.as_view(), name='about'), 
     path('', HomePageView.as_view(), name='home'),
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -1,9 +1,16 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 
-class AboutPageView(TemplateView): 
+class AboutPageView(LoginRequiredMixin,TemplateView): 
     template_name = 'about.html'
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect'
+
+
+class GraphsPageView(TemplateView): 
+    template_name = 'graphs/graphs_base.html'
