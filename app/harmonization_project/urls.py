614ca7d737b9eabef43c8afe151cc6b053f72156
Nicolas Agostini <n.b.agostini@gmail.com>
2020-04-17 08:44:12 -0400

    User admin app

--- app/harmonization_project/urls.py
+++ app/harmonization_project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,8 +7,14 @@ from upload.views import image_upload
 
 urlpatterns = [
     path("", image_upload, name="upload"),
+
+    # User management
+    path('accounts/', include('allauth.urls')),
+
+    # Django Admin
     path("admin/", admin.site.urls),
 ]
 
 if bool(settings.DEBUG):
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
