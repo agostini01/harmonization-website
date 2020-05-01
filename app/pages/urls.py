b6a59df848fc0311760bbc35c75de095e03f31d3
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-01 11:20:47 -0400

    Update graph plotting

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('graphs/', GraphsPageView.as_view(), name='graphs'), 
     path('about/', AboutPageView.as_view(), name='about'), 
     path('', HomePageView.as_view(), name='home'),
+    path('getsbdata/', GraphsPageView.getSBData),
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -1,5 +1,17 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+col=['sepal_length','sepal_width','petal_length','petal_width','type']
+iris=pd.read_csv("staticfiles/datasets/iris.csv",names=col)
+iris_setosa=iris.loc[iris["type"]=="Iris-setosa"]
+iris_virginica=iris.loc[iris["type"]=="Iris-virginica"]
+iris_versicolor=iris.loc[iris["type"]=="Iris-versicolor"]
 
 
 class HomePageView(TemplateView):
@@ -16,3 +28,12 @@ class GraphsPageView(TemplateView):
     template_name = 'graphs/graphs_base.html'
     login_url = '/accounts/login/'
     redirect_field_name = 'redirect'
+
+
+    def getSBData(request):
+        #gr=sb.factorplot(x='Survived', hue='Sex', data=df, col='Pclass', kind='count')
+        gr=sns.pairplot(iris,hue="type",height=3)
+        response = HttpResponse(content_type="image/jpg")
+        gr.savefig(response, format="jpg")
+
+        return response
