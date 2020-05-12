ffa9d586fa0acea499d8e83ad79d5b72da0bc9c2
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-12 16:40:01 -0400

    Remove image upload from upload app

--- app/harmonization_project/urls.py
+++ app/harmonization_project/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from upload.views import image_upload
-
 urlpatterns = [
     # Django Admin
     path("admin/", admin.site.urls),
