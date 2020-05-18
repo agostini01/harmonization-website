56ae75eedf83299a3f57a17eadce4a39d5176dc1
Nicolas Agostini <n.b.agostini@gmail.com>
2020-05-18 19:31:48 -0400

    Move requests functions to separate module

--- app/upload/views.py
+++ app/upload/views.py
@@ -1,54 +1,10 @@
 from django.views.generic import TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseRedirect
-
-from .forms import UploadFileForm
-
 from django.core.files.storage import default_storage
 
-import requests
-
-
-def upload_file(uploader_name, uploader_email, dataset_type, f):
-    url = "http://api:8888/query/dataset-upload/"
-
-    payload = {'uploader_name': uploader_name,
-               'uploader_email': uploader_email,
-               'dataset_type': dataset_type}
-
-    files = [
-        ('dataset_file', f.open(mode='rb'))
-    ]
-    print(files)
-    headers = {
-    }
-
-    response = requests.request(
-        "POST", url, headers=headers, data=payload, files=files)
-
-    return response
-
-
-def handle_flowers_file(uploader_name, uploader_email, dataset_type, f):
-    #default_storage.save('datasets/flowers/flowers.csv', f)
-
-    response = upload_file(uploader_name, uploader_email, dataset_type, f)
-    return response
-
-
-def handle_unm_file(f):
-    # TODO
-    default_storage.save('datasets/unm/unm.csv', f)
-
-
-def handle_neu_file(f):
-    # TODO
-    default_storage.save('datasets/neu/neu.csv', f)
-
-
-def handle_dartmouth_file(f):
-    # TODO
-    default_storage.save('datasets/dartmouth/dartmouth.csv', f)
+from .forms import UploadFileForm
+from .tools.uploads import *
 
 
 class UploadSuccessPageView(LoginRequiredMixin, TemplateView):
